1b6fa20bec0c20c42c1fe431f859cc644b7ab8e9
Ngoc Suong <suong.nguyen.fe231201@stechvn.org>
2024-04-06 23:17:09 +0700

    fix build

--- src/pages/home/components/RandomMeal.tsx
+++ src/pages/home/components/RandomMeal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import Meal from '../../../components/Meal'
 import { fetchRandomMeal } from '../../../redux/reducers/mealReducer'
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks'
