01f3d9bca3005beaf0bafb281e5af1114ec6cf97
Ngoc Suong <suong.nguyen.fe231201@stechvn.org>
2024-04-20 11:08:08 +0700

    Add firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAeZfUlwPekEC8kcPRrUvkJaJRgiciaGDA",
+    authDomain: "bae-recipe-v2.firebaseapp.com",
+    projectId: "bae-recipe-v2",
+    storageBucket: "bae-recipe-v2.appspot.com",
+    messagingSenderId: "854857465569",
+    appId: "1:854857465569:web:b180baae68780c0edb41a0",
+    measurementId: "G-HN6MB9GLYN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
+
+// https://www.youtube.com/watch?v=WpIDez53SK4
--- src/pages/home/components/RandomMeal.tsx
+++ src/pages/home/components/RandomMeal.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import Meal from '../../../components/Meal'
 import { fetchRandomMeal } from '../../../redux/reducers/mealReducer'
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks'
+import Button from '../../../components/Button'
+import { FaShuffle } from 'react-icons/fa6'
 
 const RandomMeal = () => {
     const dispatch = useAppDispatch();
@@ -17,11 +19,14 @@ const RandomMeal = () => {
     return (
         <>
             {randomMeal &&
-                <Meal
-                    meal={randomMeal}
-                    isRandom={!!randomMeal}
-                    isFav={!!favMeals.find(fav => fav.idMeal === randomMeal.idMeal)}
-                />
+                <>
+                    <Button icon={<FaShuffle />} />
+                    <Meal
+                        meal={randomMeal}
+                        isRandom={!!randomMeal}
+                        isFav={!!favMeals.find(fav => fav.idMeal === randomMeal.idMeal)}
+                    />
+                </>
             }
         </>
     );
