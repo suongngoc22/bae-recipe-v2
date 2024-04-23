2f2b9ce2f536ccbf63f3070c6ce0659bc1fec321
Ngoc Suong <suong.nguyen.fe231201@stechvn.org>
2024-04-23 20:18:33 +0700

    Add skeleton random meal and meal detail

--- src/pages/home/components/RandomMeal.tsx
+++ src/pages/home/components/RandomMeal.tsx
@@ -4,11 +4,13 @@ import { fetchRandomMeal } from '../../../redux/reducers/mealReducer'
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks'
 import Button from '../../../components/Button'
 import { FaShuffle } from 'react-icons/fa6'
+import Skeleton from '../../../components/Skeleton'
 
 const RandomMeal = () => {
     const dispatch = useAppDispatch();
     const randomMeal = useAppSelector(state => state.meal.randomMeal);
     const favMeals = useAppSelector(state => state.meal.favMeals);
+    const isLoading = useAppSelector(state => state.meal.isLoading);
 
     useEffect(() => {
         dispatch(fetchRandomMeal());
@@ -17,19 +19,23 @@ const RandomMeal = () => {
     }, [dispatch]);
 
     return (
-        randomMeal &&
-        <div className='relative'>
-            <Button
-                icon={<FaShuffle size={18} color="white" />}
-                onClick={() => dispatch(fetchRandomMeal())}
-                className="absolute z-10 top-3 right-2 w-8 h-8 bg-primary flex items-center justify-center rounded-full shadow-xl"
-            />
-            <Meal
-                meal={randomMeal}
-                isRandom={!!randomMeal}
-                isFav={!!favMeals.find(fav => fav.idMeal === randomMeal.idMeal)}
-            />
-        </div>
+        isLoading ? <Skeleton
+            active image={{ styles: 'w-full h-[426px] rounded-md' }}
+        />
+            :
+            randomMeal &&
+            <div className='relative'>
+                <Button
+                    icon={<FaShuffle size={18} color="white" />}
+                    onClick={() => dispatch(fetchRandomMeal())}
+                    className="absolute z-10 top-3 right-2 w-8 h-8 bg-primary flex items-center justify-center rounded-full shadow-xl"
+                />
+                <Meal
+                    meal={randomMeal}
+                    isRandom={!!randomMeal}
+                    isFav={!!favMeals.find(fav => fav.idMeal === randomMeal.idMeal)}
+                />
+            </div>
     );
 }
 
--- src/pages/meal/Meal.tsx
+++ src/pages/meal/Meal.tsx
@@ -5,7 +5,7 @@ import { fetchMealDetail, mealActions } from "../../redux/reducers/mealReducer"
 import Button from "../../components/Button"
 import { FiBookmark, FiChevronLeft } from "react-icons/fi"
 import SubTitle from "./components/SubTitle"
-import MealSkeleton from "./components/MealSkeleton"
+import Skeleton from "../../components/Skeleton"
 
 const MealDetail = () => {
     const { id } = useParams();
@@ -47,30 +47,29 @@ const MealDetail = () => {
     }
 
     return (
-        <>
-            {isLoading ? <MealSkeleton /> :
-                meal ?
-                    <div className="">
-                        <div className="relative">
-                            <img
-                                src={meal.strMealThumb}
-                                alt={meal.strMeal}
-                                className="w-full max-h-52 object-cover object-center z-10"
-                            />
-                            <Button
-                                icon={<FiChevronLeft size={20} color="white" />}
-                                onClick={() => navigate(-1)}
-                                className="absolute top-6 left-4 w-8 h-8 bg-primary flex items-center justify-center rounded-full shadow-xl"
-                            />
-                            <Button
-                                icon={
-                                    <FiBookmark
-                                        size={26}
-                                        stroke="white"
-                                        style={{ fill: isFav ? `white` : '' }}
-                                    />}
-                                onClick={() => handleButtonFav()}
-                                className="absolute bottom-0 right-8 translate-y-1/2 w-14 h-14 bg-primary flex items-center justify-center rounded-full 
+        isLoading ? <Skeleton paragraph={{ rows: 4, styles: 'px-8 py-5' }} image={{ styles: 'w-full h-52' }} active />
+            : meal ?
+                <div className="">
+                    <div className="relative">
+                        <img
+                            src={meal.strMealThumb}
+                            alt={meal.strMeal}
+                            className="w-full max-h-52 object-cover object-center z-10"
+                        />
+                        <Button
+                            icon={<FiChevronLeft size={20} color="white" />}
+                            onClick={() => navigate(-1)}
+                            className="absolute top-6 left-4 w-8 h-8 bg-primary flex items-center justify-center rounded-full shadow-xl"
+                        />
+                        <Button
+                            icon={
+                                <FiBookmark
+                                    size={26}
+                                    stroke="white"
+                                    style={{ fill: isFav ? `white` : '' }}
+                                />}
+                            onClick={() => handleButtonFav()}
+                            className="absolute bottom-0 right-8 translate-y-1/2 w-14 h-14 bg-primary flex items-center justify-center rounded-full 
                                 
                                     after:absolute
                                     after:content-['']
@@ -83,34 +82,32 @@ const MealDetail = () => {
                                     after:shadow-primary
                                     after:-z-10
                                     "
-                            />
-                        </div>
-                        <div className="px-8 py-5">
-                            <h1 className="text-[25px] font-bold text-left pb-4 min-w-[300px] max-w-[280px]">
-                                {meal.strMeal}
-                            </h1>
-                            <div className="flex flex-col gap-3">
-                                <SubTitle name='Ingredients' />
-                                {getIngredients(meal)?.length &&
-                                    <ul>
-                                        {getIngredients(meal).map((ingredient) => (
-                                            <li className="text-lightgray">
-                                                {ingredient}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                }
-                                <SubTitle name='Directions' />
-                                <p className="whitespace-pre-wrap text-lightgray">{meal.strInstructions}</p>
-                            </div>
+                        />
+                    </div>
+                    <div className="px-8 py-5">
+                        <h1 className="text-[25px] font-bold text-left pb-4 min-w-[300px] max-w-[280px]">
+                            {meal.strMeal}
+                        </h1>
+                        <div className="flex flex-col gap-3">
+                            <SubTitle name='Ingredients' />
+                            {getIngredients(meal)?.length &&
+                                <ul>
+                                    {getIngredients(meal).map((ingredient) => (
+                                        <li className="text-lightgray">
+                                            {ingredient}
+                                        </li>
+                                    ))}
+                                </ul>
+                            }
+                            <SubTitle name='Directions' />
+                            <p className="whitespace-pre-wrap text-lightgray">{meal.strInstructions}</p>
                         </div>
                     </div>
-                    :
-                    <div>
-                        <span>Oops sth wrong</span>
-                    </div>
-            }
-        </>
+                </div>
+                :
+                <div>
+                    <span>Oops sth wrong</span>
+                </div>
     )
 }
 
