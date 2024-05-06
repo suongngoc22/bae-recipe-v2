b8091ff91e57bae0e99c341250c8b9f548262779
suongngoc22 <nnguyensuong22@gmail.com>
2024-05-06 22:04:37 +0700

    Change z index

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
     const navigate = useNavigate();
     return (
         <div className="w-full h-screen bg-onboarding flex justify-center items-end pb-32 before:absolute before:bottom-0 before:w-full before:h-1/2 before:bg-gradient-to-t before:from-black before:to-[#0000]">
-            <div className="flex flex-col gap-8 items-center text-white text-center z-10">
+            <div className="flex flex-col gap-8 items-center text-white text-center z-11">
                 <h1 className="text-[56px] leading-snug font-semibold max-w-[247px]">Let’s
                     Cooking</h1>
                 <p className="text-[18px] font-normal leading-normal">Find best recipes for cooking</p>
